d580bd50ba39d437d6335cfe9427c9c7dc82df1c
John-John Tedro <udoprog@tedro.se>
2024-05-25 16:29:27 +0200

    Fix warnings and lints

--- src/convert.rs
+++ src/convert.rs
@@ -22,9 +22,6 @@ pub(crate) fn copy_wstring_lossy(dest: &mut [u16], source: &str) {
 }
 
 pub(crate) trait ToWide {
-    /// Encode into a wide string.
-    fn to_wide(&self) -> Vec<u16>;
-
     /// Encode into a null-terminated wide string.
     fn to_wide_null(&self) -> Vec<u16>;
 }
@@ -33,11 +30,6 @@ impl<T> ToWide for T
 where
     T: AsRef<OsStr>,
 {
-    #[inline]
-    fn to_wide(&self) -> Vec<u16> {
-        self.as_ref().encode_wide().collect()
-    }
-
     #[inline]
     fn to_wide_null(&self) -> Vec<u16> {
         self.as_ref().encode_wide().chain(Some(0)).collect()
@@ -49,11 +41,6 @@ where
     Self: Sized,
 {
     fn from_wide(wide: &[u16]) -> Self;
-
-    fn from_wide_null(wide: &[u16]) -> Self {
-        let len = wide.iter().take_while(|&&c| c != 0).count();
-        Self::from_wide(&wide[..len])
-    }
 }
 
 impl FromWide for std::ffi::OsString {
